27960ebe58d825ffd6cf72aea2cfaefcafb5567c
huangguangda <2397923107@qq.com>
2017-11-25 16:14:24 +0800

    28完成

--- app/src/main/java/cn/edu/gdmec/android/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/cn/edu/gdmec/android/photogallery/PhotoGalleryFragment.java
@@ -11,6 +11,9 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -41,6 +44,7 @@ public class PhotoGalleryFragment extends Fragment{
     public void onCreate(Bundle savedInstanceState){
         super.onCreate ( savedInstanceState );
         setRetainInstance ( true );
+        setHasOptionsMenu ( true );
         new FetchItemsTask ().execute (  );
 
         Handler responseHandler = new Handler (  );
@@ -86,6 +90,13 @@ public class PhotoGalleryFragment extends Fragment{
         mThumbnailDownloader.quit ();
         Log.i ( TAG, "Background thread destroyed" );
     }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater){
+        super.onCreateOptionsMenu ( menu, menuInflater );
+        menuInflater.inflate ( R.menu.fragment_photo_gallery, menu );
+    }
+
     //添加setupAdapter
     private void setupAdapter(){
         if (isAdded ()){
